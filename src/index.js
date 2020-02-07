996e24ba3dd578080804866eda1ded55951d26b2
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2020-02-07 15:05:49 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 
 import AsyncSelect from 'react-select/async';
 
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 function loadOptions(inputValue, callback) {
   console.log('searching', inputValue);
   fetch(`https://api-adresse.data.gouv.fr/search/?q=${inputValue.replace(/ /g, '+')}&limit=10`).then(r => r.json()).then(r => {
     callback(r.features.map(f => ({ label: f.properties.label, value: f })));
   });
 };
+
+const debLoadOptions = debounce(loadOptions, 300);
 
 export class SearchPostalAddress extends Component {
 
@@ -33,7 +50,7 @@ export class SearchPostalAddress extends Component {
         defaultValue={this.state.inputValue}
         inputValue={this.state.inputValue}
         onChange={this.onChange}
-        loadOptions={loadOptions}
+        loadOptions={debLoadOptions}
         onInputChange={this.handleInputChange}
       />
     );
